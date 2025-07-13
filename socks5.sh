730b9d4f158b8b813f51fa7043103651263320e9
Dmsrynprsty <128109589+Dmsrynprsty@users.noreply.github.com>
2025-07-13 12:10:37 +0700

    Update socks5.sh

--- socks5.sh
+++ socks5.sh
@@ -15,15 +15,23 @@ read -s -p "Enter password: " pass
 echo ""
 echo "========================================"
 
-useradd -m $user >/dev/null 2>&1
+useradd -M -s /usr/sbin/nologin $user >/dev/null 2>&1
 echo "$user:$pass" | chpasswd
+
+# Tambahkan user ke DenyUsers jika belum ada
+if ! grep -q "^DenyUsers" /etc/ssh/sshd_config; then
+    echo "DenyUsers $user" >> /etc/ssh/sshd_config
+else
+    sed -i "/^DenyUsers/ s/$/ $user/" /etc/ssh/sshd_config
+fi
+systemctl restart ssh
 
 iface=$(ip route get 1.1.1.1 | awk '{print $5; exit}')
 vpsip=$(curl -s ifconfig.me)
 
 cat > /etc/danted.conf <<EOF
 logoutput: syslog
-internal: $iface port = 8443
+internal: 0.0.0.0 port = 8443
 external: $iface
 method: username
 user.privileged: root
